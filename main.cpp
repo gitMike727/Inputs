e46446d44208ba18039010d6301cac32e346e0dd
mtruong727 <mtruong727@outlook.com>
2025-06-03 10:21:01 -0500

    Complete inputs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -7,16 +8,21 @@ int main()
     std::cin >> age;
 
     std::cin.clear();// to clear the error, tell the buffer that everything is ok
-    std::cin.ignore( 100000, '\n' ); //ignore the next amount of characters or until hit a new line
+    //31std::cin.ignore( 100000, '\n' ); //ignore the next amount of characters or until hit a new line
 
     std::string firstName;
     std::string lastName;
-    std::cout<< "Please enter your name: " << std::endl;
+    std::cout << "Please enter your name: " << std::endl;
     std::cin >> firstName;
     std::cin >> lastName;
 
+    std::string gender;
+    std::cout << "What is your gender? Male or Female: " << std::endl;
+    std::cin >> gender;
+
     std::cout << "First & Last name: " << firstName << " " << lastName << std::endl;
     std::cout << "Age: " << age << std::endl;
+    std::cout << "Gender: " << gender << std::endl;
 
 
     
